2d8d04c2c1aa858131c1696119f4590635a0f0f9
Ye Jingchen <ye.jingchen@gmail.com>
2014-04-20 16:10:33 +0800

    增加了使用状态机模型的外环模式，更新了 README

--- src/robot2014.ino
+++ src/robot2014.ino
@@ -1,3 +1,21 @@
+/*
+   本程序适用于 ATmega8 Arduino 控制版，可以完成创意机器人大赛的要求功能。
+   程序包含多个模式，根据需要设置不同的 MODE，即可完成不同的工作。
+   部分 #define 语句以后会保存到单独的文件中，请留意常量名。
+*/
+
+#define WAI_HUAN 1
+#define NEI_HUAN 2
+#define CHAO_CHE 3
+#define XUAN_YA_LE_MA 4
+#define FU_JIA_TI 5
+// 设置模式：可用值有	WAI_HUAN	NEI_HUAN	CHAO_CHE
+//			（外环）	（内环）	（超车）
+//			XUAN_YA_LE_MA	FU_JIA_TI
+//			（悬崖勒马）	（附加题）
+// 各功能尚在开发中
+#define MODE WAI_HUAN
+
 #define LED 13
 
 // 左轮和右轮
@@ -9,8 +27,8 @@
 // 6 个方向的红外 LED、光电三极管
 #define IR_OUT_BELOW_LEFT 2
 #define IR_SENSOR_BELOW_LEFT 5
-
 /*
+未确定安装在哪个管脚
 #define IR_OUT_BELOW_RIGHT
 #define IR_SENSOR_BELOW_RIGHT
 
@@ -20,26 +38,28 @@
 #define IR_OUT_RIGHT
 */
 
-#define SENSOR IR_SENSOR_BELOW_LEFT
-#define FORWARD 1	// 在 motor () 中被使用
+#define SENSOR IR_SENSOR_BELOW_LEFT // 用于 collect_IR_data ()
+
+#define FORWARD 1	// 车轮方向，在 motor () 中被使用
 #define BACK 0
-
-#define LEFT 1
+#define LEFT 1	// 左右轮，用于 motor ()
 #define RIGHT 2
-
 #define NORMAL_SPEED 250
 #define HALF_SPEED 125
 
-#define IR_BELOW_MAX 600 // <<< 需要修正
-// #define IR_FRONT_MAX
+#define IR_BELOW_MAX 600	// <<< 需要测试！
+// #define IR_FRONT_CLOSE	// <<< 需要测试！
+// #define IR_LEFT_CLOSE	// <<< 需要测试！
+// #define IR_LEFT_FAR	// <<< 需要测试！
 
-int IR_sensor_below_1;	// 在 collect_IR_data 中被使用
-int IR_sensor_below_2;
+unsigned int IR_sensor_below_1;	// 在 collect_IR_data 中被使用
+unsigned int IR_sensor_below_2;
 
 boolean LED_on = true;
 
 void motor (int side, int dir, int speed)
 {
+	// 控制车轮转动
 	// 注意！小心车轮的导线颜色，不要接反。可以用电池测试出哪根是正极
 	if (side == LEFT)
 	{
@@ -71,9 +91,9 @@ void LED_flash (void)
 
 void collect_IR_data (void)
 {
-	// collect env light data
+	// 收集环境光
 	IR_sensor_below_1 = analogRead (SENSOR);
-	Serial.println ("=====================================================");
+	Serial.println ("===================================================");
 	Serial.print ("environment light:\t");
 	Serial.println (IR_sensor_below_1);
 
@@ -84,7 +104,7 @@ void collect_IR_data (void)
 	Serial.println (IR_sensor_below_2);
 
 	Serial.print ("IR light:\t\t");
-	Serial.println (IR_sensor_below_2 - IR_sensor_below_1); // send the IR light data
+	Serial.println (IR_sensor_below_2 - IR_sensor_below_1); // 发送红外数据
 
 	delay (300);
 }
@@ -117,6 +137,7 @@ void motor_test (void)
 
 void xuan_ya_le_ma (void)
 {
+	// 悬崖勒马模式，施工中
 	if (analogRead (SENSOR) < IR_BELOW_MAX)
 	{
 		motor (LEFT, FORWARD, NORMAL_SPEED);
@@ -129,21 +150,65 @@ void xuan_ya_le_ma (void)
 	}
 }
 
-void nei_huan (void)
-{
-	// 用于内环赛道的模式，功能包括遇障停车、红灯停以及沿车道行驶
-	if (analogRead (IR_SENSOR_FRONT) < IR_FRONT_MAX)
-	{
-		motor (LEFT, FORWARD, 0);
-		motor (RIGHT, FORWARD, 0);
-	}
+///// 以下为外环模式内容
+//
+// 外环状态机状态
+#ifdef MODE == WAI_HUAN
+#define S_FORWARD 1
+#define S_STOP 2
+#define S_TURN_LEFT 3
+#define S_TURN_RIGHT 4
+#endif
 
-	if (analogRead (IR_SENSOR_RIGHT) > IR_RIGHT_MIN)
+int state;
+
+int wai_huan_detect_state (void)	// 检测当前状态
+{
+	if (analogRead (IR_SENSOR_FRONT) < IR_FRONT_CLOSE)
+		// 前方障碍物，或遇到红灯
+		return = S_STOP;
+	else if (analogRead (IR_SENSOR_LEFT) < IR_LEFT_CLOSE)
+		// 离左挡板太近
+		return = S_TURN_RIGHT;
+	else if (analogRead (IR_SENSOR_LEFT) > IR_LEFT_FAR)
+		// 离左挡板太远
+		return = S_TURN_LEFT;
+	else return = S_FORWARD;
+}
+
+void wai_huan (void)
+{
+	// 用于外环赛道的模式，功能包括遇障停车、红灯停以及沿左车道行驶
+	// 简陋的状态机版本
+	state = wai_huan_detect_state ();
+
+	switch (state)
 	{
-		motor (LEFT, FORWARD, NORMAL_SPEED);
-		motor (RIGHT, FORWARD, HALF_SPEED);
-	}
-}		
+		case S_FORWARD:
+			motor (LEFT, FORWARD, NORMAL_SPEED);	// 控制电机直行
+			motor (RIGHT, FORWARD, NORMAL_SPEED);
+			break;
+		case S_STOP:
+			motor (LEFT, FORWARD, 0);	// 停车
+			motor (RIGHT, FORWARD, 0);
+			break;
+		case S_TURN_LEFT:
+			motor (LEFT, FORWARD, HALF_SPEED);	// 掉转车头向左
+			motor (RIGHT, FORWARD, NORMAL_SPEED);
+			delay (618);	// 需要测试！
+			state = S_FORWARD;
+			break;
+		case  S_TURN_RIGHT:
+			motor (LEFT, FORWARD, NORMAL_SPEED);	// 掉转车头向右
+			motor (RIGHT, FORWARD, HALF_SPEED);
+			delay (618);	// 需要测试！
+			state = S_FORWARD;
+			break;
+		default:
+			break;
+}
+//
+///// 以上为外环模式内容
 
 void setup (void)
 {
