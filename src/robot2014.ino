1f582ea8de75da5a2bbfdc67bc837ef2f75753a2
Ye Jingchen <ye.jingchen@gmail.com>
2014-04-20 10:00:12 +0800

    some little function adjusting, added README.md

--- src/robot2014.ino
+++ src/robot2014.ino
@@ -8,8 +8,12 @@
 #define IR_OUT_BELOW 2		// IR out below pin
 #define IR_SENSOR_BELOW 5	// IR sensor below pin
 
-#define FORWARD 1	// used in left_motor () and right_motor ()
+#define FORWARD 1	// used in motor ()
 #define BACK 0
+
+#define LEFT 1
+#define RIGHT 2
+
 #define NORMAL_SPEED 250
 
 #define IR_BELOW_MAX 600
@@ -19,31 +23,34 @@ int IR_sensor_below_2;
 
 boolean LED_on = true;
 
-void left_motor (int dir, int speed)
+void motor (int side, int dir, int speed)
 {
-	if (dir == FORWARD)
+	if (side == LEFT)
 	{
-		digitalWrite (ML_DIR, 1);
-  		analogWrite (ML_SPEED, speed);
+		if (dir == FORWARD)
+		{
+			digitalWrite (ML_DIR, 1);
+  			analogWrite (ML_SPEED, speed);
+		}
+		else if (dir == BACK)
+		{ 
+			digitalWrite (ML_DIR, 0); 
+			analogWrite (ML_SPEED, speed);
+		}
 	}
-	else if (dir == BACK)
-	{ 
-		digitalWrite (ML_DIR,0); 
-		analogWrite (ML_SPEED,speed);
-	}
-}
 
-void right_motor (int dir, int speed)
-{
-	if (dir == FORWARD)
+	else if (side == RIGHT)
 	{
-		digitalWrite (MR_DIR, 1);
-  		analogWrite (MR_SPEED, speed);
-	}
-	else if (dir == BACK)
-	{ 
-		digitalWrite (MR_DIR,0); 
-		analogWrite (MR_SPEED,speed);
+		if (dir == FORWARD)
+		{
+			digitalWrite (MR_DIR, 1);
+  			analogWrite (MR_SPEED, speed);
+		}
+		else if (dir == BACK)
+		{ 
+			digitalWrite (MR_DIR, 0); 
+			analogWrite (MR_SPEED, speed);
+		}
 	}
 }
 
@@ -66,16 +73,16 @@ void collect_IR_data (void)
 	// collect env light data
 	IR_sensor_below_1 = analogRead (IR_SENSOR_BELOW);
 	Serial.println ("=====================================================");
-	Serial.println ("environment light: ");
+	Serial.print ("environment light: ");
 	Serial.println (IR_sensor_below_1);
 
 	digitalWrite (IR_OUT_BELOW, HIGH); // turn on IRout to get ready to collect IR data
 	delay (10);
 	IR_sensor_below_2 = analogRead (IR_SENSOR_BELOW);
-	Serial.println ("env + IR light: ");
+	Serial.print ("env + IR light: ");
 	Serial.println (IR_sensor_below_2);
 
-	Serial.println ("IR light: ");
+	Serial.print ("IR light: ");
 	Serial.println (IR_sensor_below_2 - IR_sensor_below_1); // send the IR light data
 
 	delay (300);
@@ -92,8 +99,8 @@ void setup ()
 	pinMode (ML_SPEED, OUTPUT);
 
 	// right motor
-	pinMode (MR_DIR,OUTPUT);
-	pinMode (MR_SPEED,OUTPUT);
+	pinMode (MR_DIR, OUTPUT);
+	pinMode (MR_SPEED, OUTPUT);
 
 	// IR sensor
 	pinMode (IR_SENSOR_BELOW, INPUT);
@@ -115,22 +122,22 @@ void setup ()
 	// motor debug
 	for (int i = 1; i <= 2; i++)
 	{
-		left_motor (FORWARD, NORMAL_SPEED);
+		motor (LEFT, FORWARD, NORMAL_SPEED);
 		delay (800);
-		left_motor (FORWARD, 0);
+		motor (LEFT, FORWARD, 0);
 		delay (100);
-		left_motor (BACK, NORMAL_SPEED);
+		motor (LEFT, BACK, NORMAL_SPEED);
 		delay (400);
-		left_motor (BACK, 0);
+		motor (LEFT, BACK, 0);
 		delay (100);
 
-		right_motor (FORWARD, NORMAL_SPEED);
+		motor (RIGHT, FORWARD, NORMAL_SPEED);
 		delay (800);
-		right_motor (FORWARD, 0);
+		motor (RIGHT, FORWARD, 0);
 		delay (100);
-		right_motor (BACK, NORMAL_SPEED);
+		motor (RIGHT, BACK, NORMAL_SPEED);
 		delay (400);
-		right_motor (BACK, 0);
+		motor (RIGHT, BACK, 0);
 		delay (100);
 
 	}
@@ -142,13 +149,13 @@ void loop ()
 
 	if (analogRead (IR_SENSOR_BELOW) < IR_BELOW_MAX)
 	{
-		left_motor (FORWARD, NORMAL_SPEED);
-		right_motor (FORWARD, NORMAL_SPEED);
+		motor (LEFT, FORWARD, NORMAL_SPEED);
+		motor (RIGHT, FORWARD, NORMAL_SPEED);
 	}
 	else 
 	{
-		left_motor (FORWARD, 0);
-		right_motor (FORWARD, 0);
+		motor (LEFT, FORWARD, 0);
+		motor (RIGHT, FORWARD, 0);
 	}
 
 	delay (200);
